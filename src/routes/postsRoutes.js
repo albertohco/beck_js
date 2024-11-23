b6feff5586d1b880677809d60bcd881a92741a92
albertohco <acordeiro@3anet.com.br>
2024-11-22 23:05:56 -0300

    inclusão método post na API

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
 import { listarUsuarios } from "../controllers/usuariosController.js";
 
 const routes = (app) => {
   app.use(express.json());
   app.get("/posts", listarPosts);
   app.get("/usuarios", listarUsuarios);
+  app.post("/posts", postarNovoPost);
 };
 
 export default routes;
